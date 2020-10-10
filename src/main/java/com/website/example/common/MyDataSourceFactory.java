c853db590f6a7bc355ff6328d513527fe122720c
Rabbitwhite <34395602+whitedodo@users.noreply.github.com>
2020-10-10 21:09:13 +0900

    Update MyDataSourceFactory.java

--- src/main/java/com/website/example/common/MyDataSourceFactory.java
+++ src/main/java/com/website/example/common/MyDataSourceFactory.java
@@ -4,7 +4,7 @@
  *  파일명(Filename): MyDataSourceFactory.java
  *  저자(Author): Dodo / rabbit.white at daum dot net
  *  비고(Description):
- *  
+ *  1. Apache DBCP2 - BasicDataSource 추가(커넥션풀) , Dodo , 2020-10-10
  */
 
 package com.website.example.common;
@@ -19,10 +19,12 @@ import javax.sql.DataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 import oracle.jdbc.pool.OracleDataSource;
+// import org.apache.commons.dbcp2.BasicDataSource;
 
 public class MyDataSourceFactory {
 	
 	private InputStream is = null;
+    
 	
 	public MyDataSourceFactory()  {
 		
@@ -78,5 +80,37 @@ public class MyDataSourceFactory {
         return oracleDS;
         
     }
+        
+    /*
+    public DataSource getBasicDataSource() {
+
+        Properties props = new Properties();
+    	// BasicDataSource ds = null;
+    	Connection conn = null;
+        
+        try {
+ 
+            props.load(is);
+ 
+            ds = new BasicDataSource();
+            ds.setDriverClassName(props.getProperty("MYSQL_DB_DRIVER_CLASS"));
+            ds.setUrl(props.getProperty("MYSQL_DB_URL"));
+            ds.setUsername(props.getProperty("MYSQL_DB_USERNAME"));
+            ds.setPassword(props.getProperty("MYSQL_DB_PASSWORD"));
+ 
+                ds.setInitialSize(10); // The initial number of connections that
+                                    // are created when the pool is started.
+            ds.setMaxTotal(20); // The maximum number of active connections
+                                    // that can be allocated from this pool
+                
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+        }
+ 
+        return conn;
+ 
+    }
+    */
+ 
 
 }
